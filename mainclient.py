b02c788c47d3e00134f6a01a678268959b3efcea
inventorxtreme <inventorxtreme@gmail.com>
2020-04-24 17:00:15 -0400

    SETUP COMMIT

---
+++ mainclient.py
@@ -0,0 +1,243 @@
+import requests
+import threading
+from tkinter import *
+import time
+import pickle
+import tkinter
+from tkinter import messagebox
+global updategood
+updategood = True
+from tkinter import simpledialog
+import os
+import webbrowser
+clientversion = "- 0.2b"
+
+
+
+global user,server,port
+try:
+    config = pickle.load(open("config.p", "rb"))
+    user = config["username"]
+    server = config["server"]
+    port = config["port"]
+    print("config loaded")
+    print(user)
+except:
+    print("CONFIG ERROR: SETTING UP")
+    namesetup = input("NAME: ")
+    serversetup = input("IP: ")
+    portsetupIN = input("port: ")
+    portsetup = ":" + portsetupIN
+    config = {"username":"Guestsadf", "server":"127.0.0.1", "port": ":69"}
+    config["server"] = serversetup
+    config["port"] = portsetup
+    config["username"] = namesetup
+    pickle.dump(config, open("config.p","wb+"))
+    config = pickle.load(open("config.p", "rb"))
+    user = config["username"]
+    server = config["server"]
+    port = config["port"]
+    print("config loaded")
+    print(user)
+
+def changename():
+    global user, server, port
+    print("CONFIG NAME SETTUP STARTED")
+    #TODO: PROMPT
+    namesetup = simpledialog.askstring("Chat Noise -> Settings -> Username", "Input New Username:")
+    config = pickle.load(open("config.p", "rb"))
+
+    config["username"] = namesetup
+
+    pickle.dump(config, open("config.p", "wb+"))
+    config = pickle.load(open("config.p", "rb"))
+    user = config["username"]
+    server = config["server"]
+    port = config["port"]
+    print("config loaded")
+    print(user)
+
+def changeserver():
+    global user, server, port
+    print("CONFIG NAME SETTUP STARTED")
+    # TODO: PROMPTs
+    namesetup = simpledialog.askstring("Chat Noise -> Settings -> Server", "Input New Server:")
+    config = pickle.load(open("config.p", "rb"))
+
+    config["server"] = namesetup
+
+    pickle.dump(config, open("config.p", "wb+"))
+    config = pickle.load(open("config.p", "rb"))
+    user = config["username"]
+    server = config["server"]
+    port = config["port"]
+    print("config loaded")
+    print(user)
+
+def changeport():
+    global user, server, port
+    print("CONFIG NAME SETTUP STARTED")
+    # TODO: PROMPT
+    namesetup = simpledialog.askstring("Chat Noise -> Settings -> Port", "Input New Port:")
+    config = pickle.load(open("config.p", "rb"))
+
+    config["port"] = ":" + namesetup
+
+    pickle.dump(config, open("config.p", "wb+"))
+    config = pickle.load(open("config.p", "rb"))
+    user = config["username"]
+    server = config["server"]
+    port = config["port"]
+    print("config loaded")
+    print(user)
+
+
+
+
+
+
+
+def loadb64image():
+    print("loaded")
+    base64_img = simpledialog.askstring("Chat Noise -> B64 Image Decoder", "Input Image Data to Decode")
+    url = "http://" + server+port+"/image/" + base64_img
+    webbrowser.open(url,new=2)
+
+
+def encodeb64image():
+    print("encoded")
+    url = "http://" + server + port + "/upimage"
+    webbrowser.open(url,new=2)
+
+
+def openfile():
+    print("openfiled")
+    os.system("out.txt")
+def sync():
+    print("sync")
+    send()
+def send_clip():
+    print("send_clip")
+    send(root.clipboard_get())
+
+def getid():
+    servboi = "http://" + server + port + "/messageid/"
+    out = requests.get(servboi)
+    return out.text
+
+def send(senddata):
+    print("sendt")
+    try:
+        outline = user + ": " + senddata
+    except TypeError:
+        messagebox.showerror("Chat Noise Config Error", "You must have a username to send a message")
+    servboi = "http://" + server + port + "?send="
+    toadd = getid()
+    added1 = int(toadd)
+    added2 = added1 + 1
+    addedout = str(added2)
+    out = servboi + outline + "&id=" + addedout
+    temp = requests.get(out)
+
+def setupdate():
+    print("what does this do")
+    global updategood
+    updategood = not updategood
+
+def sendread(a):
+    print("read")
+    try:
+        send(chatbox.get())
+        chatbox.delete(0,END)
+    except requests.exceptions.ConnectionError:
+        tkinter.messagebox.showerror(title="NetError", message="Connection Error sending data to server")
+def sendreadb():
+    sendread("f")
+
+def about():
+    print("abouted")
+    try:
+        url = "http://" + server + port + "/ver"
+        temp = requests.get(url)
+        serverversion = temp.text
+    except:
+        serverversion = "Error connecting to server"
+
+    messagebox.showinfo(title="About", message="ChatNoise -> A chat client/protocol for the people\n"
+                                               "Developed By: Joe Mama\n"
+                                               "Client Version " + clientversion +""
+                                                                                 " using Tkinter GUI\n"
+                                              "Server Version: "+ serverversion)
+
+
+root = Tk()
+root.configure(background='grey10')
+Title = "Python Chat Noise Client " + clientversion
+root.title(Title)
+root.bind('<Return>',sendread)
+text = Text(root,width=80,height=20,bg="grey10",fg="white")
+text.pack()
+chatboxframe = Frame(root,bg="grey10")
+chatboxframe.pack()
+chatbox = Entry(chatboxframe,width = 100,bg="grey10",fg="white")
+chatbox.pack(side = LEFT)
+sendb = Button(chatboxframe,command = sendreadb,text="send",bg="grey10",fg="red3")
+sendb.pack(side=RIGHT)
+
+menubar = Menu(root)
+
+
+settingsmenu = Menu(root)
+
+settingsmenu.add_command(label="Change Username", command=changename)
+settingsmenu.add_command(label="Change Server", command=changeserver)
+settingsmenu.add_command(label="Change Port", command=changeport)
+
+codemenu = Menu(root)
+
+codemenu.add_command(label="Encode",command=encodeb64image)
+codemenu.add_command(label="Decode",command=loadb64image)
+
+FileMenu = Menu(menubar)
+
+#FileMenu.add_command(label="Settings",command=settings)
+
+FileMenu.add_command(label="Enable/Disable Refresh",command=setupdate)
+FileMenu.add_command(label="Send Clipboard",command=send_clip)
+FileMenu.add_command(label="About",command=about)
+menubar.add_cascade(label="File",menu=FileMenu)
+menubar.add_cascade(label="Encode/Decode Images",menu=codemenu)
+menubar.add_cascade(label="Settings", menu=settingsmenu)
+root.config(menu=menubar)
+
+def update_display():
+    print("display")
+    iservboi = "http://" + server + port + "?get"
+    try:
+        down = requests.get(iservboi)
+    except requests.exceptions.ConnectionError:
+        return "Error Connecting to server"
+    x = down.text
+    with open("chatlogclient.txt", "w+") as file:
+        file.write(x)
+    with open("chatlogclient.txt", 'r') as fin:
+        return fin.read()
+
+
+def refresh(h):
+    print("refeshed")
+    while True:
+        if updategood != False:
+            x = update_display()
+            text.delete('1.0', END)
+            text.insert(END, x)
+            text.see(END)
+            time.sleep(3)
+
+
+
+
+GuiLoop = threading.Thread(target=refresh, args=(1,),daemon=True)
+GuiLoop.start()
+
+root.mainloop()
---
+++ mainserver.py
@@ -0,0 +1,129 @@
+import flask
+from flask import request, Response
+import requests
+import base64
+from flask import Flask, request, redirect, send_file
+from werkzeug.utils import secure_filename
+import os
+import imghdr
+servversion = "S0.2b"
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+ALLOWED_EXTENSIONS = set(['jpg', 'png', 'jpeg', 'gif'])
+UPLOAD_PATH = './image'
+app.config['UPLOAD_PATH'] = UPLOAD_PATH
+global messageid
+messageid = 0
+
+global msgsync
+msgsync = 0
+global servname
+servname = "ERROR TIME"
+global syncserver1
+syncserver1 = "http://192.168.86.75:69"
+global syncname1
+syncname1 = "MACMACMAC1"
+
+def allowed_file(_file):
+    image_type = imghdr.what(None, _file.read())
+    return image_type in ALLOWED_EXTENSIONS
+
+@app.route("/ver")
+def version():
+    return servversion
+
+@app.route('/image/<image_name>', methods=['GET'])
+def get_image(image_name):
+    if image_name is not None:
+        image_name = secure_filename(image_name)
+        image = open(os.path.join(app.config['UPLOAD_PATH'], image_name))
+        god_christ_help = ".\image/" + image_name
+        return send_file(god_christ_help)
+    else:
+        return 'error'
+
+@app.route('/messageid/')
+def messageidget():
+    return str(messageid)
+
+@app.route('/upimage/', methods=['GET', 'POST'])
+def upload_image():
+    if request.method == 'POST':
+        _file = request.files['file']
+        if _file:
+            filename = secure_filename(_file.filename)
+            # because imghdr.what() reads file to end, must set file's position 0.
+            _file.seek(0)
+            try:
+                _file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+            except IOError:
+                os.mkdir(app.config['UPLOAD_PATH'])
+                _file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+
+            return redirect('/image/' + filename)
+        else:
+            return "Upload failed."
+    else:
+        return '<form action="." method="post" enctype="multipart/form-data"><input type="file" name="file" /><button type="submit">Upload</button></form>'
+
+
+
+try:
+    chatlogtt = open("chatlog.txt",'a')
+    chatlogtt.close()
+except FileNotFoundError:
+    print("File not accessible")
+
+
+def send1(senddata):
+
+    outline = "SERVNAME" + ": " + senddata
+
+    out = ""+syncserver1+"?send="+senddata
+    # out = "http://127.0.0.1:5000?send=" + outline
+    temp = requests.get(out)
+
+def sync():
+    global syncname1
+    with open('chatlog.txt', 'r') as f:
+        synclist = [line.strip() for line in f]
+    for x in range(msgsync):
+        gamer = synclist[::-1]
+        prebound = synclist.pop(-1 )
+        outbound = servname +" " + prebound
+        if prebound[0:10] != syncname1:
+            print(outbound)
+            send1(outbound)
+
+
+
+
+@app.route('/', methods=['GET'])
+def home():
+    global  messageid
+    global msgsync
+    if "send" in request.args:
+        if int(request.args['id']) > messageid:
+            chatlog = open("chatlog.txt", 'a')
+            message = request.args['send']
+            vermessage = message + '\n'
+            chatlog.write(vermessage)
+            chatlog.close()
+            messageid += 1
+            return "writen"
+        else:
+            return 'ID ERROR'
+
+    elif "get" in request.args:
+        with open("chatlog.txt",'r') as fin:
+            return Response(fin.read(), mimetype='text/plain')
+    elif "ret" in request.args:
+        sync()
+        return "hek"
+
+    else:
+        return "no request"
+
+
+app.run(host = "0.0.0.0", port = 69)
+
